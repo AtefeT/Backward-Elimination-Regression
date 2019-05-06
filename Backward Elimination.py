3bc993d082c8e649ac7e50fb68215aed1410b47d
Atefe <a.toobak@gmail.com>
2019-05-07 00:06:52 +0200

    Add files via upload

---
+++ Backward Elimination.py
@@ -0,0 +1,16 @@
+#Backward Elimination
+#sl  significant Level
+#OLS  Ordinary least Square 
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
